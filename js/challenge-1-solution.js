7a03070b934551287483dc353450ae592f8cc0e8
ayaqabbani <ayaqabbani998@gmail.com>
2021-03-30 13:37:49 +0300

    solved

---
+++ js/challenge-1-solution.js
@@ -0,0 +1,6 @@
+'use strict';
+const arr = [6,4,3,2];
+function calcHypotenuse(a , b) {
+    return (Math.sqrt((a * a) + (b * b)));
+  }
+  console.log(calcHypotenuse(arr[i]));
---
+++ js/challenge-2-solution.js
@@ -0,0 +1,13 @@
+'use strict';
+let number1 = prompt("provide first number");
+let number2 = prompt("provide second number");
+let number3 = prompt("provide third number");
+let number4 = prompt("provide fourth number");
+let number5 = prompt("provide fifth number");
+
+parent(number1);
+parent(number2);
+parent(number3);
+parent(number4);
+parent(number5);
+alert (Math.max(number1,number2,number3,number4,number5)(Math.min(number1,number2,number3,number4,number5)));
